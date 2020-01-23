ac42ae62a8ac0e71089d974ad87ee516255f17c3
KN <kylenguyen@Kyles-MacBook-Pro-2.local>
2020-01-23 11:48:47 -0800

    Style updates to schema file

--- database/index.js
+++ database/index.js
@@ -5,17 +5,17 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to airbnb database!');
-})
+});
 
-var ImageSchema = new Schema({id: Number, image_link: String});
+var ImageSchema = new Schema({id: Number, imageLink: String});
 
-var relatedSchema = new mongoose.Schema({
+var RelatedSchema = new mongoose.Schema({
   id: Number,
   images: [ImageSchema],
-  home_category: String,
-  bed_count: Number,
-  listing_title: String,
-  star_count: Number,
-  review_count: Number,
-  price_per_night: Number
+  homeCategory: String,
+  bedCount: Number,
+  listingTitle: String,
+  starCount: Number,
+  reviewCount: Number,
+  pricePerNight: Number
 });
