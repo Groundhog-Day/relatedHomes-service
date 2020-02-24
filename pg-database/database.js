2b9b7457e1add8b5e40630940b82620aa69b2381
rolopez23 <rob.lopez@gmail.com>
2020-02-24 14:55:27 -0800

    Updates db for user on EC2

--- pg-database/database.js
+++ pg-database/database.js
@@ -2,7 +2,7 @@ const { Pool, Client } = require('pg');
 const parseHomes = require('./parseData.js');
 
 const client = new Client({
-    user: 'robertlopez',
+    user: 'postgres',
     host: 'localhost',
     database: 'relatedhomes',
     password: '',
